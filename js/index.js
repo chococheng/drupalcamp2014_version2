71725acc7ed5876fb91d0b8a3745c42a61eb874a
Lisa Cheng <lisacheng@LisadeMacBook-Pro.local>
2014-06-24 13:39:48 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$('document').ready(function() {
+	var options = { videoId: 'hKzMw1mdb-g', start: 3 };
+	$('#wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+
+});
